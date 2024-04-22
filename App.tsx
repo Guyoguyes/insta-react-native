9089ba64ead72c84641cce82d8c74694e338f5c0
Guyoguyes <g.abduba43@gmail.com>
2024-04-23 00:23:10 +0300

    commit

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import Home from './pages/home/home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
+  
   return (
-    <PaperProvider>
-      <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <PaperProvider>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
     </PaperProvider>
+    </NavigationContainer>
+    
     
   );
 }
---
+++ pages/home/home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+import { Text } from 'react-native-paper'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic
+} from "@expo-google-fonts/roboto";
+
+function Home() {
+  return (
+    <SafeAreaView>
+
+      <Text>Hello</Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = Styles
+
+export default Home
